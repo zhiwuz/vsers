d55b62003498c583ec53a3cd36ddc687ba40a3d9
zhiwuz <zhwzheng.1994@gmail.com>
2021-01-10 18:51:27 -0500

    Format coding styles

--- camera_reconstruct/cameraReconstruct.py
+++ camera_reconstruct/cameraReconstruct.py
@@ -1,29 +1,36 @@
 import numpy as np
 
+
 class cameraReconstructor(object):
+
     def __init__(self):
         self.cameraIntrinsics = np.identity(3)
         self.rotation = np.identity(3)
-        self.transition = np.zeros((3,1))
-        
-    def reset(self, cameraIntrinsics = None, rotation = None, transition = None):
-        if not cameraIntrinsics is None:
+        self.transition = np.zeros((3, 1))
+
+    def reset(self, cameraIntrinsics=None, rotation=None, transition=None):
+        if cameraIntrinsics is not None:
             self.cameraIntrinsics = cameraIntrinsics
-        if not rotation is None:
+        if rotation is not None:
             self.rotation = rotation
-        if not transition is None:
+        if transition is not None:
             self.transition = transition
-      
+
     def reconstruct(self, pixX, pixY):
         cameraIntrinsics = self.cameraIntrinsics
         inputDepth = 1.0
-        camX = (pixX-cameraIntrinsics[0,2])*inputDepth/cameraIntrinsics[0,0]
-        camY = (pixY-cameraIntrinsics[1,2])*inputDepth/cameraIntrinsics[1,1]
+        camX = (pixX -
+                cameraIntrinsics[0, 2]) * inputDepth / cameraIntrinsics[0, 0]
+        camY = (pixY -
+                cameraIntrinsics[1, 2]) * inputDepth / cameraIntrinsics[1, 1]
         camZ = 0.0
         invRotation = np.linalg.inv(self.rotation)
-        worldX = invRotation[0,0] * camX + invRotation[0,1] * camY + invRotation[0,2] * camZ + self.transition[0,0]
-        worldY = invRotation[1,0] * camX + invRotation[1,1] * camY + invRotation[1,2] * camZ + self.transition[1,0]
-        worldZ = invRotation[2,0] * camX + invRotation[2,1] * camY + invRotation[2,2] * camZ + self.transition[2,0]
+        worldX = invRotation[0, 0] * camX + invRotation[
+            0, 1] * camY + invRotation[0, 2] * camZ + self.transition[0, 0]
+        worldY = invRotation[1, 0] * camX + invRotation[
+            1, 1] * camY + invRotation[1, 2] * camZ + self.transition[1, 0]
+        worldZ = invRotation[2, 0] * camX + invRotation[
+            2, 1] * camY + invRotation[2, 2] * camZ + self.transition[2, 0]
         coordinate = np.transpose(np.array((worldX, worldY, worldZ)))
-        
+
         return coordinate
---
+++ detect_track/objectDetect.py
@@ -0,0 +1,84 @@
+import cv2 as cv
+import imutils
+import numpy as np
+import matplotlib.pyplot as plt
+from camera_reconstruct.cameraReconstruct import cameraReconstructor
+
+
+class objectDetector(object):
+
+    def __init__(self, maxNumObjects=1):
+        self.maxNumObjects = maxNumObjects
+        self.reconstructor = cameraReconstructor()
+        self.croppedRect = None
+
+    # set the region of interest for cropping
+    def set_cropped_rect(self, croppedRect):
+        self.croppedRect = croppedRect
+
+    def set_reconstructor(self,
+                         cameraIntrinsics=None,
+                         rotation=None,
+                         transition=None):
+        self.reconstructor.reset(cameraIntrinsics=cameraIntrinsics,
+                                 rotation=rotation,
+                                 transition=transition)
+
+    def plot_color_image(self, colorimg):
+        plt.imshow(colorimg)
+
+    def crop_image(self, inputImg, croppedRect):
+        if isinstance(croppedRect, type(None)):
+            return inputImg
+        assert type(croppedRect) == np.ndarray
+        [x, y, dx, dy] = croppedRect.astype('int')
+        img = inputImg[y:y + dy, x:x + dx]
+        return img
+
+    # detect the centroid of the objects in the image, output are the coordinates of the centroids, and a list of opencv contour objects
+    def centroid_detection(self, image, maxNumObjects):
+        image = image.copy()
+        image = image.astype('uint8')
+        blurred = cv.GaussianBlur(image, (5, 5), 0)
+        thresh = cv.threshold(blurred, 0, 255,
+                              cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+        cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL,
+                               cv.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        cnts = [cnt for cnt in cnts if cv.contourArea(cnt) > 5.0]
+        cnts = sorted(cnts, key=lambda x: -cv.contourArea(x))
+        filteredCount = min(len(cnts), maxNumObjects)
+        cnts = cnts[:filteredCount]
+        centroids = np.zeros((filteredCount, 2), dtype=int)
+        for cntId in range(len(cnts)):
+            cnt = cnts[cntId]
+            M = cv.moments(cnt)
+            cx = int(M['m10'] / M['m00'])
+            cy = int(M['m01'] / M['m00'])
+            centroids[cntId, 0] = cx
+            centroids[cntId, 1] = cy
+        return centroids, cnts
+
+    def detection_plot(self, image, centroids, plot=True):
+        image = image.copy()
+        if len(image.shape) == 2:
+            image = cv.cvtColor(image, cv.COLOR_GRAY2RGB)
+        for centroid in centroids:
+            cv.circle(image, tuple(centroid.astype('int')), 5, (1.0, 0.0, 0.0),
+                      -1)
+        if plot:
+            plt.imshow(image)
+        return image
+
+    # the main method for object detection
+    def detect(self, inputColor, plot=True):
+        croppedRect = self.croppedRect
+        maxNumObjects = self.maxNumObjects
+        croppedInputColor = self.crop_image(inputColor, croppedRect)
+        centroids, _ = self.centroid_detection(croppedInputColor, maxNumObjects)
+        image = self.detection_plot(croppedInputColor, centroids, plot)
+        coordinates = centroids
+        coordinates = self.reconstructor.reconstruct(
+            croppedRect[0] + coordinates[:, 0],
+            croppedRect[1] + coordinates[:, 1])
+        return coordinates, centroids, croppedInputColor, image
